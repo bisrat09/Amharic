7ab4b9331ccb56a2c86761589a3f3e02446a39c4
bisrat <bisrat09@gmail.com>
2018-11-13 16:12:06 -0800

    second commit

---
+++ app/src/main/java/com/example/bisrat/liguamharic/Model.java
@@ -0,0 +1,27 @@
+package com.example.bisrat.liguamharic;
+
+
+
+// constructs model object with title and icon field
+// for adapter and view to display
+public class Model {
+
+
+    String title;
+    int icon;
+    // constructor
+    public Model(String title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    // returns title
+    public String getTitle() {
+        return this.title;
+    }
+
+    // returns icon
+    public int getIcon() {
+        return this.icon;
+    }
+}
